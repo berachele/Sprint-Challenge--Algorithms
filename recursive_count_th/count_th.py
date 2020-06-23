53c05b0797749839680bc7158473eefccbcfb20b
Rachele Edwards <dot.com1472@gmail.com>
2020-06-23 11:17:15 -0600

    PASSING ALL 5 TESTS

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,19 +8,12 @@ def count_th(word):
     count = 0
     if len(word) == 0:
         return 0
-    elif word[0] == 't':
+    elif word[0] == 't' and len(word) > 1:
         if word[1] == 'h':
-            # print('has H afterwards, adding to count')
             return count+1 + count_th(word[1:])
         else: 
-            # print('hitting else else, recurse again')
             return count_th(word[1:])
     else:
-        # print(f'not "th" sequence, RECURSING AGAIN --> word[1:]: {word[1:]}')
         return count_th(word[1:])
 
-    # print(f'count: {count}')
     return count
-
-# word = "THtHThth"
-# print(count_th(word))
